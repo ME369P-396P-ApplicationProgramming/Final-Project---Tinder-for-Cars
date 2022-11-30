934c3c8882940de7df575587b9fe323a3a77f647
long-hannah <112027982+long-hannah@users.noreply.github.com>
2022-11-30 14:38:32 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-import streamlit as st
-import pandas as pd
-st.set_page_config(page_title = "Car Tinder")
-st.sidebar.success("Pages")
-st.title('Car Tinder')
-price = st.slider("Price", min_value = 0, max_value = 100000, step = 1000)
-mileage = st.text_input("Mileage")
+import cartest
 
-zipcode = st.text_input("Zip Code")
-radius = st.slider("Radius", min_value = 0, max_value = 500, step = 1)
-submit_button = st.button("SubmitX", on_click)
-
-info = [price,mileage,zipcode,radius]
-
-def out_info():
-    print(info)
-    
-if submit_button:
-    print(info)
+def inputs(info):
+    price =  info[0] 
+    mileage =  info[1] 
+    zipcode =  info[2] 
+    radius =  info[3] 
+    cool = 0
+    return cool,price,mileage,zipcode,radius,cool
